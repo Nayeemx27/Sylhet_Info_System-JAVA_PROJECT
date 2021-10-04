6fa1e94c08fb92412bc2acbabe14924263a265b3
Aftar Ahmad Sami <82672770+Aftar-Ahmad-Sami@users.noreply.github.com>
2021-10-04 19:06:36 +0600

    Update Tourism.java

--- Tourism.java
+++ Tourism.java
@@ -2,5 +2,29 @@ class Tourism{
 	int display()
 	{
 		System.out.println("...");
+        System.out.println("\t\t\t1.Hazrat Shajalal Mazar ");
+        System.out.println("\t\t\t2.Lalakhal");
+        System.out.println("\t\t\t3.Lakkatura");
+        System.out.println("\t\t\t4.Madhobkhundo Jolopropat");
+
+
+        System.out.println("-----------------");
+        System.out.println("Discription:The Shah Jalal Dargah (Bengali: শাহজালাল দরগাহ) is the shrine and burial place of the 14th century Muslim saint Shah Jalal,\n located in Sylhet, Bangladesh. The site, known as a dargah, was originally constructed c. 1500,\n though many additions and alterations were made to its structures over the following centuries.");
+        System.out.println("place: Chowhatta ");
+        System.out.println("Howtogo:You have to take the rickshaw or the automatic CNG rickshaw and head towards Dargah Mahallah from Sylhet Sadar.\nHow to get there: Sylhet Distric:There are three types of transportation modes available to Sylhet from Dhaka. You can get there by bus, train or plane.");
+        System.out.println("Explore:Sha Jalal's Mazar, Jalali Kobutor, Gozar Mach(Fish)");
+        System.out.println("365 days you can visit ");
+        System.out.println("Cordinate:24.902464058107388, 91.86593847988624");
+        System.out.println("-----------------");
+
+
+        System.out.println("Discription:Lalakhal is a wide channel in the Sharee River near the Tamabil road.\n The river is not very deep and is one of the sources of sand in Sylhet. The focal point of the feature is the variety of colours of the water,\n which varies from blue to green to clear at different points.");
+        System.out.println("Place:Jaintpur upozilla");
+        System.out.println("Howtogo:Tourists usually travel to Sarighat, which is 42 km away from Sylhet city centre, by road and then hire a boat to reach Lalakhal. The one-and-a-quarter hour (mechanised-) boat ride over this crystal green water with lush green hills on both sides of the river is a soothing sight for any nature lovers.\n" +
+                "\n" +
+                "There is a boat station in Sarighat operated by Nazimgarh Resort.Different types of mechanised boats can be hired from there to travel to Lalakhal.");
+        System.out.println("Explore:The beauty of nature");
+        System.out.println("June to Spetember");
+        System.out.println("Cordiante:25.107168064805492, 92.17666622377611");
 	}
 }
