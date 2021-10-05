184baeee153d0bcbd166511082768aa63c9dd69f
Nayeemx27 <88242287+xRadon27@users.noreply.github.com>
2021-10-05 15:50:59 +0600

    Update Tourism.java
    Done KhadimNagar
    Jaflong
    Ratargul
    Bisanakandi
    Srimangal.
    
    Check and let me know

--- Tourism.java
+++ Tourism.java
@@ -27,5 +27,113 @@ class Tourism{
      //   System.out.println("Explore:The beauty of nature");
         System.out.println("Best Time To Travel: June to Spetember");
         System.out.println("Cordiante:25.107168064805492, 92.17666622377611");
+		
+		
+		
+		//NAYEEM (o_o)
+		
+		// Jaflong
+
+		System.out.println("Jaflong");
+		System.out.println("");
+		System.out.println("Description:");
+		System.out.println(
+				"Jaflong (Bengali: জাফলং) is a hill station and tourist destination in the Division of Sylhet, Bangladesh. It is located in Gowainghat Upazila of Sylhet District and situated at the border between Bangladesh and the Indian state of Meghalaya, overshadowed by subtropical mountains and rainforests. It is known for its stone collections and is home of the Khasi tribe.");
+		System.out.println();
+		System.out.println("Best time to visit:");
+		System.out.println("During winter and rainy season.");
+		System.out.println("");
+		System.out.println("Places to Explore: ");
+		System.out.println("1. Zero Point");
+		System.out.println("2.Piain River");
+		System.out.println("3.Sengram Punji Waterfalls");
+		System.out.println("4.Khasia Village");
+		System.out.println("5.Jaflong Bridge");
+		System.out.println("6.Stone Collection activities");
+		System.out.println("");
+		System.out.println("Way to Reach the destination:");
+		System.out.println(
+				"You can go to Jaflong by CNG or Auto with BDT 1200 to 2000. If you take Microbus, it will cost BDT 3000 to 5000 for the whole day. From any places of Sylhet city, you will find CNG or Auto Rickshaw. If you are in a group, the best and affordable way is going with Microbuses.");
+		System.out.println("");
+		System.out.println("Coordinates:");
+		System.out.println("25.1634° N, 92.0175° E");
+		System.out.println("");
+
+		// Bichnakandi
+
+		System.out.println("Bichnakandi");
+		System.out.println("");
+		System.out.println("Description: ");
+		System.out.println(
+				"Bichnakandi (Bengali: বিছানাকান্দি, Bichhanakandi), also known as Bisnakandi, is a village in Rustompur Union, Gowainghat Upazila of Bangladesh's Sylhet District.[1][2] In recent years, there has been an influx of tourists to its river.");
+		System.out.println("");
+		System.out.println("Best Time to Visit:");
+		System.out.println("During rainy season");
+		System.out.println("");
+		System.out.println("Way to Reach the destination:");
+		System.out.println(
+				"To go to Bisnakandi, first of all, you have to visit Amberkhana Point in Sylhet City. There is a CNG station on Airport Road. It is best to reserve CNG and go to a place called Hadarpar. From here, you can boat or even you can go by walking to Bisnakandi.");
+		System.out.println("");
+		System.out.println("Coordinates:");
+		System.out.println("25.1695° N, 91.8867° E");
+		System.out.println("");
+
+		// Ratargul Swamp Forest
+
+		System.out.println("Ratargul Swamp Forest");
+		System.out.println("");
+		System.out.println("Description:");
+		System.out.println(
+				"Ratargul Swamp Forest is a freshwater swamp forest located in Gowain River, Fatehpur Union, Gowainghat, Sylhet, Bangladesh. It is the only swamp forest located in Bangladesh and one of the few freshwater swamp forests in the world. The forest is naturally conserved under the Department of Forestry, Government of Bangladesh.");
+		System.out.println("");
+		System.out.println("Best Time to Visit:");
+		System.out.println("During rainy season.");
+		System.out.println("");
+		System.out.println("Way to Reach the Destination:");
+		System.out.println(
+				"To visit Ratargul Swamp Forest, you need to go to Sylhet city first. From Sylhet, take a CNG (mini taxi) to Goain Ghat. It will take 2.5 – 3 hours time and 450-500 BDT fare. From Goain Ghat need to reserve a trawler (local engine boat) to the forest.");
+		System.out.println("");
+		System.out.println("Coordinates:");
+		System.out.println("25.0142° N, 91.9187° E");
+		System.out.println("");
+
+		// Srimangal
+
+		System.out.println("Srimangal");
+		System.out.println("");
+		System.out.println("Description:");
+		System.out.println(
+				"Sreemangal is situated in Moulvibazar district in sylhet division. Sreemangal is an Upazila. It is famous for tea garden. Rain all time occurs here. Nature has adorned sreemangal with green tress. Its natural scenery is very charming. It soothes one’s eyes. Birds are twittering always here.");
+		System.out.println("");
+		System.out.println("Best Time to Visit:");
+		System.out.println("During rainy season");
+		System.out.println("");
+		System.out.println("Way to Reach the Destination:");
+		System.out.println(
+				"As Srimongol is not along the main road between Sylhet and Dhaka, the bus is not really the best option to reach it. However,train is a efficient way of travel. You can buy your ticket by yourself but try to buy it in advance. It will take 2 and half hours to reach from Sylhet. Also, you can hire a car from sylhet. It will cost 3000-4000 BDT for full day tour.");
+		System.out.println("");
+		System.out.println("Coordinates:");
+		System.out.println("24.3065° N, 91.7296° E");
+		System.out.println("");
+
+		// Khadim Nagar National Park
+
+		System.out.println("Khadim Nagar National Park");
+		System.out.println("");
+		System.out.println("Description:");
+		System.out.println(
+				"(Bengali: খাদিমনগর জাতীয় উদ্যান) is a major national park and nature reserve in Bangladesh. The park is located at Sylhet Sadar Upazila, Sylhet District in the North-East region of the country. It is located mainly on the Hills and is surrounded by Kalagool, Bhurjan and Goolni tea estates. Khadim Nagar National Park covers approximately 679 ha (6.79 km2) of evergreen forests Biome.");
+		System.out.println("");
+		System.out.println("Best Time to Visit:");
+		System.out.println(
+				"November to February is considered the best time to visit Khadimnagar National Park, as the climate is humid and lukewarm.");
+		System.out.println("");
+		System.out.println("Way to Reach the Destination: ");
+		System.out.println(
+				"Visitors coming from Sylhet City can hire a CNG auto rickshaw from anywhere, 7 km from Sylhet City on Tamabil Highway. The Park is a further 5 km on the left.Although the road is in fairly decent condition and not a problem for vehicles, it is recommended to hire an auto rickshaw on rainy days");
+		System.out.println("");
+		System.out.println("Coordinates:");
+		System.out.println("24.9570° N, 91.9344° E");
+		System.out.println("");
 	}
 }
